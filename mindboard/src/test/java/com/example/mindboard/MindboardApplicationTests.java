60fce7b9d0a773d9e404fed5e77317aaee99c4e5
Lucas Sena <159453926+01Lucasena@users.noreply.github.com>
2025-06-23 20:44:36 -0300

    Add files via upload

---
+++ mindboard/src/test/java/com/example/mindboard/MindboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mindboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
